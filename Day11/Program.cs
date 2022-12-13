9e32896bb179aeb1e7589df3fc4bf47a0680b5cd
davide della casa venturelli <davide.dellacasaventurelli@gmail.com>
2022-12-13 21:17:56 +0100

    Fix day11

--- Day11/Program.cs
+++ Day11/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System.Diagnostics;
-using System.Numerics;
 
 namespace Day11
 {
@@ -9,21 +8,23 @@ namespace Day11
         Add
     }
 
-    record MonkeyWorryLevelOperation(MonkeyWorryLevelOperationType Type, uint? OperationValue);
+    record MonkeyWorryLevelOperation(MonkeyWorryLevelOperationType Type, int? OperationValue);
 
-    record MonkeyItemTest(uint TestDivisor, uint MokeyIdWhenTrue, uint MonkeyIdWhenFalse);
+    record MonkeyItemTest(int TestDivisor, int MokeyIdWhenTrue, int MonkeyIdWhenFalse);
 
     class KeepAwayState
     {
         public int RoundNumber { get; private set; }
+        public int Mcd { get; }
         public IReadOnlyCollection<Monkey> Monkeys { get; }
 
-        public KeepAwayState(IReadOnlyCollection<Monkey> monkeys)
+        public KeepAwayState(IReadOnlyCollection<Monkey> monkeys, int mcd)
         {
             Monkeys = monkeys;
+            Mcd = mcd;
         }
 
-        public void ExecuteRound(bool includeRelief)
+        public void ExecuteRound()
         {
             // Monkey turn
             foreach (var monkey in Monkeys)
@@ -31,16 +32,9 @@ namespace Day11
                 // Throw all the items in the inventory one by one
                 while (monkey.Inventory.Count > 0)
                 {
-                    (uint, Item)? inspectionResult;
+                    (int, Item)? inspectionResult;
 
-                    if (includeRelief)
-                    {
-                        inspectionResult = monkey.InspectItemPart1();
-                    }
-                    else
-                    {
-                        inspectionResult = monkey.InspectItemPart2();
-                    }
+                    inspectionResult = monkey.InspectItemPart(Mcd);
 
                     if (inspectionResult != null)
                     {
@@ -56,26 +50,26 @@ namespace Day11
 
     class Monkey
     {
-        public uint Id { get; }
+        public int Id { get; }
         private readonly Queue<Item> _inventory = new();
 
         public IReadOnlyCollection<Item> Inventory => _inventory;
 
         public ulong Inspections { get; private set; }
 
-        private readonly MonkeyWorryLevelOperation _worryLevelOperation;
-        private readonly MonkeyItemTest _itemTest;
+        public MonkeyWorryLevelOperation WorryLevelOperation { get; }
+        public MonkeyItemTest ItemTest { get; }
 
         public Monkey(
-            uint id,
+            int id,
             MonkeyWorryLevelOperation worryLevelOperation,
             MonkeyItemTest itemTest,
             IReadOnlyCollection<Item> initialInventory
             )
         {
             Id = id;
-            _worryLevelOperation = worryLevelOperation;
-            _itemTest = itemTest;
+            WorryLevelOperation = worryLevelOperation;
+            ItemTest = itemTest;
 
             foreach (Item item in initialInventory)
             {
@@ -93,9 +87,7 @@ namespace Day11
             _inventory.Enqueue(item);
         }
 
-        #region Part1
-
-        public (uint, Item)? InspectItemPart1()
+        public (int, Item)? InspectItemPart(int mcd)
         {
             if (_inventory.Count == 0)
             {
@@ -104,133 +96,59 @@ namespace Day11
 
             var item = _inventory.Dequeue();
 
-            var newWorryLevel = CalculateNewWorryLevelPart1(item);
+            var newWorryLevel = CalculateNewWorryLevelPart(item, mcd);
 
             item.WorryLevel = newWorryLevel;
 
-            var newMoneyId = DecideWhoToThrowToPart1(item);
+            var newMoneyId = DecideWhoToThrowToPart(item);
 
             Inspections++;
 
             return (newMoneyId, item);
         }
 
-        private uint CalculateNewWorryLevelPart1(Item item)
+        private ulong CalculateNewWorryLevelPart(Item item, int mcd)
         {
             // The old value is encoded as -1
-            uint value = _worryLevelOperation.OperationValue != null ? _worryLevelOperation.OperationValue.Value : item.WorryLevel;
+            ulong value = WorryLevelOperation.OperationValue != null ? (ulong)WorryLevelOperation.OperationValue.Value : item.WorryLevel;
 
-            var newWorryLevel = _worryLevelOperation.Type switch
+            var newWorryLevel = WorryLevelOperation.Type switch
             {
                 MonkeyWorryLevelOperationType.Add => item.WorryLevel + value,
-                MonkeyWorryLevelOperationType.Mul => (double)(item.WorryLevel * value),
+                MonkeyWorryLevelOperationType.Mul => (item.WorryLevel * value),
                 _ => throw new NotSupportedException(),
             };
 
+            // For part1
             //Divided by 3 and rounded for part one. The value was double but now is long for part two.
-            newWorryLevel /= 3.0;
-            return (uint)Math.Floor(newWorryLevel);
+            //newWorryLevel /= 3.0;
+            //return (int)Math.Floor(newWorryLevel);
+
+            return newWorryLevel % (ulong)mcd;
         }
 
-        private uint DecideWhoToThrowToPart1(Item item)
+        private int DecideWhoToThrowToPart(Item item)
         {
             // Assume the test is always a division
-            if (item.WorryLevel % _itemTest.TestDivisor == 0)
+            if (item.WorryLevel % (ulong)ItemTest.TestDivisor == 0)
             {
-                return _itemTest.MokeyIdWhenTrue;
+                return ItemTest.MokeyIdWhenTrue;
             }
             else
             {
-                return _itemTest.MonkeyIdWhenFalse;
+                return ItemTest.MonkeyIdWhenFalse;
             }
         }
 
-        #endregion
-
-        #region Part 2
-
-        public (uint, Item)? InspectItemPart2()
-        {
-            if (_inventory.Count == 0)
-            {
-                return null;
-            }
-
-            var item = _inventory.Dequeue();
-
-            var newMoneyId = DecideWhoToThrowToPart2(item);
-
-            Inspections++;
-
-            return (newMoneyId, item);
-        }
-
-        // TODO: there are bugs to fix
-        private uint DecideWhoToThrowToPart2(Item item)
-        {
-            BigInteger modA;
-
-            if (item.ModWorryLevels.ContainsKey(_itemTest.TestDivisor))
-            {
-                modA = item.ModWorryLevels[_itemTest.TestDivisor];
-            }
-            else
-            {
-                modA = item.WorryLevel % _itemTest.TestDivisor;
-                item.ModWorryLevels.Add(_itemTest.TestDivisor, modA);
-            }
-
-            var mobB = _worryLevelOperation.OperationValue != null ? new BigInteger(_worryLevelOperation.OperationValue.Value % _itemTest.TestDivisor) : modA;
-
-            if (_worryLevelOperation.Type == MonkeyWorryLevelOperationType.Add)
-            {
-                var modC = (modA + mobB) % _itemTest.TestDivisor;
-                item.ModWorryLevels[_itemTest.TestDivisor] = modC;
-
-                if (modC == 0)
-                {
-                    return _itemTest.MokeyIdWhenTrue;
-                }
-                else
-                {
-                    return _itemTest.MonkeyIdWhenFalse;
-                }
-
-            }
-            else if (_worryLevelOperation.Type == MonkeyWorryLevelOperationType.Mul)
-            {
-                var modC = (modA * mobB) % _itemTest.TestDivisor;
-                item.ModWorryLevels[_itemTest.TestDivisor] = modC;
-
-                if (modC == 0)
-                {
-                    return _itemTest.MokeyIdWhenTrue;
-                }
-                else
-                {
-                    return _itemTest.MonkeyIdWhenFalse;
-                }
-            }
-            else
-            {
-                throw new NotSupportedException();
-            }
-        }
-
-        #endregion
     }
 
     class Item
     {
-        // Modified in part 1, read only in part 2
-        public uint WorryLevel { get; set; }
+        public ulong WorryLevel { get; set; }
 
-        // Used only by part 2
-        public Dictionary<uint, BigInteger> ModWorryLevels { get; } = new();
-
-        public Item(uint initialWorryLevel)
+        public Item(int initialWorryLevel)
         {
-            WorryLevel = initialWorryLevel;
+            WorryLevel = (ulong)initialWorryLevel;
         }
     }
 
@@ -243,8 +161,8 @@ namespace Day11
             // 7 lines = 1 monkey
             foreach (var lines in File.ReadLines("input.txt").Chunk(7))
             {
-                var monkeyId = Convert.ToUInt32(lines[0].Trim().Split()[1].Replace(":", string.Empty));
-                var items = lines[1].Trim().Split().Skip(2).Select(a => new Item(Convert.ToUInt32(a.Trim().Replace(",", string.Empty)))).ToArray();
+                var monkeyId = Convert.ToInt32(lines[0].Trim().Split()[1].Replace(":", string.Empty));
+                var items = lines[1].Trim().Split().Skip(2).Select(a => new Item(Convert.ToInt32(a.Trim().Replace(",", string.Empty)))).ToArray();
 
                 var line2Split = lines[2].Trim().Split();
 
@@ -257,11 +175,11 @@ namespace Day11
 
                 var valueRaw = line2Split[5];
 
-                var value = line2Split[5] == "old" ? (uint?)null : Convert.ToUInt32(line2Split[5]);
+                var value = line2Split[5] == "old" ? (int?)null : Convert.ToInt32(line2Split[5]);
 
-                var divisor = Convert.ToUInt32(lines[3].Trim().Split()[3]);
-                var trueCondition = Convert.ToUInt32(lines[4].Trim().Split()[5]);
-                var falseCondition = Convert.ToUInt32(lines[5].Trim().Split()[5]);
+                var divisor = Convert.ToInt32(lines[3].Trim().Split()[3]);
+                var trueCondition = Convert.ToInt32(lines[4].Trim().Split()[5]);
+                var falseCondition = Convert.ToInt32(lines[5].Trim().Split()[5]);
 
 
                 monkeys.Add(new Monkey(monkeyId, new MonkeyWorryLevelOperation(operation, value), new MonkeyItemTest(divisor, trueCondition, falseCondition), items));
@@ -280,14 +198,15 @@ namespace Day11
 
             stopWatch.Restart();
 
-            var state = new KeepAwayState(monkeys);
+            var mcd = monkeys.Select(a => a.ItemTest.TestDivisor).Aggregate((total, current) => total * current);
 
-            var rounds = 20;
-            bool includeRelief = false;
+            var state = new KeepAwayState(monkeys, mcd);
+
+            var rounds = 10000;
 
             for (int i = 0; i < rounds; i++)
             {
-                state.ExecuteRound(includeRelief);
+                state.ExecuteRound();
             }
 
             var topTwoActiveMonkeys = state.Monkeys.OrderByDescending(a => a.Inspections).Take(2);
